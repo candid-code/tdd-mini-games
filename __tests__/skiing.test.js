38dc9466a257a21b25d329f4dc8d0e5c2867ab3f
Andrew Burgess <andrew@burgess.io>
2020-09-10 22:46:55 -0400

    can make a path wider

--- __tests__/skiing.test.js
+++ __tests__/skiing.test.js
@@ -1,4 +1,4 @@
-const { getNextRow } = require('../src/skiing');
+const { getNextRow, PathChoice } = require('../src/skiing');
 
 describe('Skiing Game', () => {
     it('creates new rows that are the same length as previous rows', () => {
@@ -7,5 +7,11 @@ describe('Skiing Game', () => {
         const nextRow = getNextRow(prevRow);
 
         expect(nextRow.length).toBe(prevRow.length);
+    });
+
+    it('returns a wider path', () => {
+        const prevRow = [0, 1, 0];
+        const nextRow = getNextRow(prevRow, PathChoice.Wider);
+        expect(nextRow).toEqual([0, 1, 1]);
     });
 });
--- src/skiing.js
+++ src/skiing.js
@@ -1,7 +1,19 @@
-function getNextRow(prevRow) {
+const PathChoice = {
+    Same: 0,
+    Wider: 1,
+    Narrower: 2,
+};
+
+function getNextRow(prevRow, pathChoice = PathChoice.Same) {
+    if (pathChoice === PathChoice.Wider) {
+        const nextRow = prevRow.slice(0);
+        nextRow[2] = 1;
+        return nextRow;
+    }
     return prevRow;
 }
 
 module.exports = {
+    PathChoice,
     getNextRow,
 };
